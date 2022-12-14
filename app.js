418d2867f36a8b5c081ce6cb6a98498fe7811fee
Abed-Nawafleh <65203366+Abed-Nawafleh@users.noreply.github.com>
2022-12-15 00:03:36 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -12,14 +12,20 @@ app.get('/', (req, res) => {
 
 app.get('/store',function(req,res){
     res.sendFile(path.join(__dirname+'/website/store.html'));
-  });
+});
 
 app.get('/img/:img',(req,res)=> {
     res.sendFile(path.join(__dirname+'/website/img/'+req.params.img));
-})
+});
 app.get('/:fileDir',(req,res) => {
     res.sendFile(path.join(__dirname+'/website/'+ req.params.fileDir));
-})
+});
+app.get('/css/:fileDir', (req,res) => {
+    res.sendFile(path.join(__dirname +'/website/css/'+req.params.fileDir));
+});
+app.get('/js/:fileDir', (res,req)=> {
+    res.sendFile(path.join(__dirname +'/website/js/'+ req.params.fileDir));
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, ()=> console.log('Listening on port 3000'));
